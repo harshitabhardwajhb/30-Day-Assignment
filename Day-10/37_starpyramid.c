9034d976fa99639bdd85ec4aaa6b57b2c64449fa
devanshbhardwajdb <bhardwajharshita035@gmail.com>
2026-06-18 18:28:04 +0530

    day-10

---
+++ Day-10/37_starpyramid.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            if (j >= 4 - i + 1 && j <= 4 + i - 1)
+            {
+                printf("*");
+            }
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
