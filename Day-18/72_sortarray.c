80c8cab63c8627060c2fc1a6ef552a05e37d96db
devanshbhardwajdb <bhardwajharshita035@gmail.com>
2026-06-26 16:15:14 +0530

    Day-18 Done

---
+++ Day-18/72_sortarray.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {9, 4, 7, 1, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Sorted array in descending order: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
